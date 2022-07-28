d82c8b7940830957be67394f86807ff001211d75
george-hub331 <idiaghegeorge9@gmail.com>
2022-07-28 16:30:53 +0100

    fix: slider

--- src/Components/SliderComponent.js
+++ src/Components/SliderComponent.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react'
 import img1 from '../assets/images/meals.png'
-import Carousel from 'better-react-carousel'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination';
+import { Pagination, EffectCoverflow } from 'swiper'
+import 'swiper/css';
 
 const SliderComponent = () => {
  
@@ -11,84 +11,89 @@ const SliderComponent = () => {
     <div className='md:mt-40 mt-20 items-center justify-center text-center font-heading'>
     <h6 className='text-sm text-secondary'>~ Popular Delivery ~</h6>
        <h2 className='text-3xl'>See what people love recently</h2>
-       <Carousel 
-       cols={6} rows={1} 
-       gap={10} loop={true} 
-       hideArrow={false} 
-       autoplay={5000}>
-        <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center md:w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Eba and Egusi</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Jollof Rice </h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Fried Rice</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Semo and Efo riro</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Rice and Beans</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Moi Moi</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Moi Moi</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Moi Moi</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Moi Moi</h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Jollof Rice </h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Jollof Rice </h4>
-           </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className=' w-fit text-start align-center rounded-md '>
-                     <img className='text-center w-[90%] p-3' src={img1}/>
-                 <h4 className='mt-0 p-3 text-tiny font-heading'> Jollof Rice </h4>
-           </div>
-      </Carousel.Item>
-    </Carousel>
+
+      <Swiper
+      effect={"coverflow"}
+      grabCursor={true}
+      centeredSlides={true}
+      slidesPerView={4}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+      }}
+      pagination={true}
+      modules={[EffectCoverflow, Pagination]}
+      className="mySwiper"
+      >
+
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center md:w-full p-3' src={img1} />
+            <h4 className='mt-0 text-tiny font-heading'> Eba and Egusi</h4>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img className='text-center w-[400px] p-3' alt={""} src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=' w-[400px] text-start align-center rounded-md '>
+            <img alt="" className='text-center w-[400px] p-3' src={img1} />
+          </div>
+        </SwiperSlide>    
+
+      </Swiper>
+
+      
   </div>
   )
 }
