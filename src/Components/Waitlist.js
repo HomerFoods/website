7e7b9852f70a2c21c153be232c3012c0e8ba8b46
Olamide Olatunde <apphle@Olamides-MacBook-Pro.local>
2022-09-01 11:16:07 +0300

    thursday

--- src/Components/Waitlist.js
+++ src/Components/Waitlist.js
@@ -4,6 +4,7 @@ import home from '../assets/images/Home.png'
 import orderdetails from '../assets/images/OrderDetails.png'
 import preorder from '../assets/images/preorder.png'
 import img1 from '../assets/images/headerhero.png'
+import waitlistn from '../assets/images/waitlistn.png'
 
 const Waitlist = () => {
 
@@ -37,19 +38,21 @@ const Waitlist = () => {
       }
 
   return (
-    <div className='bg-secondary font-poppins mt-6 pb-20'>
+    <div className='bg-white font-poppins md:mt-2 mt-6 pb-'>
        <div className='flex flex-col items-center justify-center text-center pt-10'>
-       <div className='md:flex md:px-10 font-heading md:mt-10 mt- mx-2'>
-     <div className='mt-10'>
+
+   {/*HEADER*/}
+       <div className='md:flex md:px-10 font-heading md:mt-10 bg-white mx-2'>
+
+     <div className='mt-10 bg-white'>
    <div className='mx-2'>
-   <h2 className='md:text-5xl text-la text-start'>Hungry ?</h2>
-        <h4 className='md:text-la text-base text-start'>Living in a foreign land and you miss home cooked meals ?</h4>
-        <p className='font-bold md:mt-5 md:text-base text-sm mt-3 text-start'> Taste home away from home with HomerFoods</p>
-
-        <p className='md:text-base text-sm text-start'>Join the waitlist now and get informed once we launch !</p>
+   <h2 className='md:text-3xl text-la text-center font-yukita text-redesign'>Taste home </h2>
+   <h2 className='md:text-3xl text-la text-center font-yukita'>away from home </h2>
+        <p className='md:text-sm text-sm text-center font-regular'>Join the waitlist now and get informed once we launch !</p>
    </div>
-        <div className='flex flex-col mt-3 items-start justify-start w-[340px] text-start'>
 
+
+        <div className='flex flex-col mt-3 items-center justify-center w-[340px] text-center'>
         <input
           className='w-full mb-3 mx-2 outline-none mt-2 md:mt-0 text-base md:text-base border hover:border-black border-[#d6d5d5] p-2 rounded-md'
           placeholder='Your Name'
@@ -62,30 +65,29 @@ const Waitlist = () => {
             value={name} onChange={(e) => setName(e.target.value)}
           />
 
-          <button className='text-white p-2 w-[80%] bg-button rounded-md mx-2' onClick={join}>Join Waitlist</button>
+          <button className='text-white p-2 w-[80%] bg-redesign rounded-md mx-2' onClick={join}>Join Waitlist</button>
         </div>
+
      </div>
-      
-      <div>
-        <img 
-        className='md:w-[90%] w-[96%] mt-6 md:mt-0'
-        src={img1}/>
-      </div>
    </div>
-        <div className='flex mx-10 justify-between mt-28'>
-            <img src={delivery}
-            className='w-[8%] h-[8%]'
-            />
-            <img src={home} 
-            className='mt-10 md:mt-20 mx-5 w-[35%] '
-            />
-            <img src={orderdetails}
-            className='mx-5 w-[30%] h-[30%] md:h-[10%] md:w-[30%]'
-            />
-            <img src={preorder} 
-             className='w-[8%] h-[8%]'/>
-        </div>
-        <div className='mt-20 items-center justify-center w-[70%] min-w-[350px] flex flex-col md:mx-10 mx-0'>
+   {/*HEADER*/}
+
+   {/*MOBILEPHONE*/}
+
+   <div className='bg-secondary mt-10 p-20 w-[100%] items-center justify-center flex'>
+     <img
+     className='md:w-[40%]'
+     src={waitlistn}
+     alt='image'
+     />
+   </div>
+
+   {/*MOBILEPHONE*/}
+  
+
+   {/*FAQ*/}
+       <div className='w-[100%] bg-secondary items-center justify-center flex md:mt-0 pb-4'>
+       <div className='mt-20 items-center justify-center w-[70%] min-w-[350px] flex flex-col md:px-4 mx-0 bg-secondary'>
            <h4 className='text-white text-3xl'>Frequently Asked Questions</h4>
            <div className='border-white border-2 rounded-md bg-faq p-3 w-full mx-10 items-center text-white mt-10'>
                   <div className='text-start flex justify-between'>
@@ -144,6 +146,9 @@ const Waitlist = () => {
            </div>
            
         </div>
+       </div>
+          {/*FAQ*/}
+
        </div>
     </div>
   )
--- src/Components/WaitlistHeader.js
+++ src/Components/WaitlistHeader.js
@@ -10,16 +10,16 @@ const WaitlistHeader = () => {
 
     const [open, setOpen] = useState(false)
   return (
-    <div className='font-poppins  w-full fixed top-0 left-0 z-40 bg-secondary'>
-        <div className='md:flex items-center justify-between  py-4 md:px-10 px-7 md:bg-secondary bg-white'>
+    <div className='font-poppins  w-full fixed top-0 left-0 z-40 bg-white'>
+        <div className='md:flex items-center justify-between  py-4 md:px-10 px-7 md:bg-white bg-white'>
             <div className='font-extrabold text-2xl cursor-pointer flex items-center text-secondary'>
                 {/* Logo is going to be in the span tag */}
-                <span className='text-2xl text-black mr-1 pt-2'> HOMERFOODS</span>
+                <span className='text-2xl text-redesign font-bold mr-1 pt-2'> HOMERFOODS</span>
             </div>
             <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-3 cursor-pointer md:hidden md: items-center pt-[-3]'>
             <ion-icon name={open ? 'close': 'menu'}></ion-icon>
             </div>
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:bg-secondary bg-white
+            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:bg-white bg-white
              md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 
              transition-all duration-500 ease-in 
              ${open ? 'top-10 opacity-100': 'hidden top-20'}  
@@ -32,7 +32,7 @@ const WaitlistHeader = () => {
                       ))
                   }
                    <div className='md:flex justify-between'>
-            <button className='bg-secondary text-white p-2 rounded-full md:w-[120%] text-2xl w-[40%] md:ml-20 ml-0'> Share </button>
+            <button className='md:bg-white bg-secondary md:text-black text-white p-2 rounded-full md:w-[120%] text-2xl w-[40%] md:ml-20 ml-0'> Share </button>
         </div>
                 </ul>
         </div>
